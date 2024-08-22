54419c2bd3ee4ef47703c6b06826ab5d129aa23c
Tertius <123890620+Tertiusva@users.noreply.github.com>
2024-08-23 01:15:02 +0200

    New Stuff

--- lib/login/membershipCategory/pages/paymentMethods.dart
+++ lib/login/membershipCategory/pages/paymentMethods.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sama/components/myutility.dart';
+import 'package:sama/login/membershipCategory/pages/ui/PaymentTextReu.dart';
 
 import '../../../components/styleButton.dart';
 
@@ -18,10 +21,109 @@ class PaymentMethod extends StatefulWidget {
 }
 
 class _PaymentMethodState extends State<PaymentMethod> {
+  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        Container(
+          width: MyUtility(context).width,
+          height: MyUtility(context).height * 0.3,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              border: Border.all(color: Colors.grey, width: 1.5)),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MyUtility(context).width * 0.32,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      PaymentTextreu(
+                          boldText: 'Application Type',
+                          secondText: 'PRIVATE PRACTICE'),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 4, 0, 4),
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 5, bottom: 5),
+                          child: PaymentTextreu(
+                              boldText: 'Category',
+                              secondText:
+                                  'Medically qualified spouse of full member'),
+                        ),
+                      ),
+                      PaymentTextreu(boldText: 'Annual', secondText: 'R4584'),
+                      Text(
+                        'Calculated on a pro-rata basis when payng annually',
+                        style: GoogleFonts.openSans(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14,
+                            color: Colors.grey[800],
+                            letterSpacing: -0.5),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      JournalCheckBox(
+                        title: 'Include SA Medical Journal (SAMJ)',
+                        onChanged: (bool value) {},
+                      )
+                    ],
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      children: [
+                        PaymentTextPr(
+                            boldText: 'Pro-rata Amount', secondText: 'R1490'),
+                        PaymentTextPr(
+                            boldText: 'For Period',
+                            secondText: 'Aug 2024 - Dec 2024'),
+                      ],
+                    ),
+                    Text(
+                      'R887',
+                      style: GoogleFonts.openSans(
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey[600],
+                          fontSize: 16,
+                          letterSpacing: -0.5),
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'Total Due',
+                          style: GoogleFonts.openSans(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey[800],
+                              fontSize: 16,
+                              letterSpacing: -0.5),
+                        ),
+                        Text(
+                          'R1490',
+                          style: GoogleFonts.openSans(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey[800],
+                              fontSize: 16,
+                              letterSpacing: -0.5),
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
---
+++ lib/login/membershipCategory/pages/ui/PaymentTextReu.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class PaymentTextreu extends StatefulWidget {
+  final String boldText;
+  final String secondText;
+
+  PaymentTextreu({super.key, required this.boldText, required this.secondText});
+
+  @override
+  State<PaymentTextreu> createState() => _PaymentTextreuState();
+}
+
+class _PaymentTextreuState extends State<PaymentTextreu> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        RichText(
+          text: TextSpan(
+            style: GoogleFonts.openSans(
+              fontSize: 16,
+              color: Colors.grey[800],
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                text: '${widget.boldText}: ',
+                style: GoogleFonts.openSans(
+                    fontWeight: FontWeight.bold, letterSpacing: -0.5),
+              ),
+              TextSpan(
+                text: widget.secondText,
+                style: GoogleFonts.openSans(
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey[600],
+                    letterSpacing: -0.5),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class PaymentTextPr extends StatefulWidget {
+  final String boldText;
+  final String secondText;
+
+  PaymentTextPr({super.key, required this.boldText, required this.secondText});
+
+  @override
+  State<PaymentTextPr> createState() => _PaymentTextPrState();
+}
+
+class _PaymentTextPrState extends State<PaymentTextPr> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        RichText(
+          text: TextSpan(
+            style: GoogleFonts.openSans(
+              fontSize: 16,
+              color: Colors.grey[800],
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                text: '${widget.boldText}: \n',
+                style: GoogleFonts.openSans(
+                    fontWeight: FontWeight.bold, letterSpacing: -0.5),
+              ),
+              TextSpan(
+                text: widget.secondText,
+                style: GoogleFonts.openSans(
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey[600],
+                    letterSpacing: -0.5),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class JournalCheckBox extends StatefulWidget {
+  final String title;
+  final bool initialValue;
+  final ValueChanged<bool> onChanged;
+
+  JournalCheckBox({
+    required this.title,
+    this.initialValue = false,
+    required this.onChanged,
+  });
+
+  @override
+  _JournalCheckBoxState createState() => _JournalCheckBoxState();
+}
+
+class _JournalCheckBoxState extends State<JournalCheckBox> {
+  late bool isChecked;
+
+  @override
+  void initState() {
+    super.initState();
+    isChecked = widget.initialValue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          isChecked = !isChecked;
+        });
+        widget.onChanged(isChecked);
+      },
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Checkbox(
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value ?? false;
+              });
+              widget.onChanged(isChecked);
+            },
+          ),
+          SizedBox(width: 8),
+          Text(
+            widget.title,
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey[700],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
