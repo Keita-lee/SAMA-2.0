4ab373ee79c301a1181a74a6afc22c232d292792
ChrisPotgieter <62018254+ChrisPotgieter@users.noreply.github.com>
2024-09-04 20:35:31 +0200

    email payments added

---
+++ lib/components/email/payments/onlinePayment.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future sendOnlineUpdate({
+  required String name,
+  required String email,
+}) async {
+  final serviceId = 'service_igwbojp';
+  final templateId = 'template_3a4tkfn';
+  final userId = 'Jmk16IabzDvgmXBeJ';
+  // createOtpVerification(email, otp);
+  final url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
+  final response = await http.post(url,
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({
+        'service_id': serviceId,
+        'template_id': templateId,
+        'user_id': userId,
+        'template_params': {
+          'member_name': name,
+          'user_email': email,
+        }
+      }));
+}
