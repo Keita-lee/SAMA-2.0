e9cd644e2595897b521fbc433a0e5ea0194f670f
ChrisPotgieter <62018254+ChrisPotgieter@users.noreply.github.com>
2024-09-05 22:20:11 +0200

    version changed

--- lib/login/loginPages/loginWithEmail.dart
+++ lib/login/loginPages/loginWithEmail.dart
@@ -645,7 +645,7 @@ class _LoginWithEmailState extends State<LoginWithEmail> {
             height: 15,
           ),
           const Text(
-            'v1.0.01d',
+            'v1.0.01e',
             style: TextStyle(
                 fontSize: 12, color: Color.fromARGB(255, 122, 122, 122)),
           )
