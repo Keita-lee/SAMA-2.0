339ad45e06675a2b8b8355a59ee2e91ef26c35c8
All in Jest <25633109+jessemart07@users.noreply.github.com>
2024-09-30 14:04:08 +0200

    update version

--- lib/login/loginPages/loginWithEmail.dart
+++ lib/login/loginPages/loginWithEmail.dart
@@ -868,7 +868,7 @@ class _LoginWithEmailState extends State<LoginWithEmail> {
               height: 15,
             ),
             const Text(
-              'v1.0.09f',
+              'v1.0.1f',
               style: TextStyle(
                   fontSize: 12, color: Color.fromARGB(255, 122, 122, 122)),
             )
